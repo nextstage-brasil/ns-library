a6eb4aa1a567b5a561936c36d3c37b1c37eabddb
Nextstage <ns@nextstage.com.br>
2022-10-04 10:38:43 -0300

    feature/Acrescido connection com SQlite também

--- src/Connection.php
+++ src/Connection.php
@@ -20,19 +20,28 @@ class Connection {
      * @return ConnectionPostgreSQL
      */
     public static function getConnection() {
-        $type = ((!Config::getData('DBTYPE')) ? 'postgres' : Config::getData('DBTYPE'));
+        $config = ((null === Config::getData('database')) ?
+                [
+            'host' => Config::getData('DBHOST'),
+            'user' => Config::getData('DBUSER'),
+            'pass' => Config::getData('DBPASS'),
+            'port' => Config::getData('DBPORT'),
+            'dbname' => Config::getData('DBNAME'),
+            'type' => Config::getData('DBTYPE'),
+                ] : Config::getData('database')
+                );
+        $type = ((!$config['type']) ? 'postgres' : $config['type']);
         if (!self::$con[$type]) {
             try {
                 switch ($type) {
                     case 'postgres':
-                        $config = Config::getData('database');
                         self::$con[$type] = new ConnectionPostgreSQL($config['host'], $config['user'], $config['pass'], $config['port'], $config['dbname']);
                         break;
                     case 'sqlite':
-                        if (null === Config::getData('DBFILENAME')) {
-                            throw new Exception("Connection config 'DBFILENAME' is not defined");
+                        if (null === $config['dbname']) {
+                            throw new Exception("Connection config 'DBNAME' is not defined");
                         }
-                        self::$con[$type] = new SQLite(Config::getData('DBFILENAME'));
+                        self::$con[$type] = new SQLite($config['dbname']);
                         break;
                     default:
                         throw new Exception("Connection $type is not enabled");
