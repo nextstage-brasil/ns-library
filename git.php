e110b4191d93e7d22281a9dde16acc212a00c253
Nextstage <ns@nextstage.com.br>
2023-05-29 18:44:03 +0200

    fix/implements firstOrFail on Dao

--- git.php
+++ git.php
@@ -1,12 +1,13 @@
 <?php
 
+use NsUtil\Helper;
+
 require 'vendor/autoload.php';
 
 if (strlen((string)$argv[1]) === 0) {
     die("Mensagem do commit é requerido");
 }
-$dir = __DIR__;
-var_export($dir);
+$dir = Helper::getPathApp();
 
 NsUtil\Package::git(__DIR__ . '/version', $argv[1]);
 
